496990938b5fbf60d6af80404603b36c449797fc
Chris813 <53151569+Chris813@users.noreply.github.com>
2023-08-20 22:49:31 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -54,11 +54,4 @@ class handler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
             return
-def run(server_class=HTTPServer, handler_class=handler, port=8000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting server on port {port}...')
-    httpd.serve_forever()
 
-if __name__ == '__main__':
-    run()
